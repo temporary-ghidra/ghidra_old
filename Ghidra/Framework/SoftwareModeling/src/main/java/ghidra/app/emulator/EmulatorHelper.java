8160b91e866d5f8bc33d879f945581fac8b2e3db
Danil6969 <20danil02@mail.ru>
2020-04-21 19:36:40 +0300

    Update EmulatorHelper.java

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/emulator/EmulatorHelper.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/app/emulator/EmulatorHelper.java
@@ -25,6 +25,7 @@ import ghidra.app.emulator.state.RegisterState;
 import ghidra.framework.store.LockException;
 import ghidra.pcode.emulate.BreakCallBack;
 import ghidra.pcode.emulate.EmulateExecutionState;
+import ghidra.pcode.memstate.ManagedMemory;
 import ghidra.pcode.memstate.MemoryFaultHandler;
 import ghidra.pcode.memstate.MemoryState;
 import ghidra.program.model.address.*;
@@ -45,6 +46,9 @@ public class EmulatorHelper implements MemoryFaultHandler, EmulatorConfiguration
 	private Register stackPtrReg;
 	private AddressSpace stackMemorySpace;
 
+	private ConstantPool cpool;
+	private ManagedMemory managedMemory;
+
 	private String lastError;
 	private MemoryWriteTracker memoryWriteTracker;
 
@@ -64,6 +68,14 @@ public class EmulatorHelper implements MemoryFaultHandler, EmulatorConfiguration
 
 		stackPtrReg = program.getCompilerSpec().getStackPointer();
 		stackMemorySpace = program.getCompilerSpec().getStackBaseSpace();
+
+		try {
+			cpool = program.getCompilerSpec().getPcodeInjectLibrary().getConstantPool(program);
+		}
+		catch (IOException e){}
+
+		if (cpool != null)
+			managedMemory = new ManagedMemory(cpool);
 
 		emulator = new Emulator(this);
 	}
@@ -102,6 +114,16 @@ public class EmulatorHelper implements MemoryFaultHandler, EmulatorConfiguration
 	@Override
 	public Language getLanguage() {
 		return program.getLanguage();
+	}
+
+	@Override
+	public ConstantPool getConstantPool() {
+		return cpool;
+	}
+
+	@Override
+	public ManagedMemory getManagedMemory() {
+		return managedMemory;
 	}
 
 	public Program getProgram() {
@@ -314,6 +336,14 @@ public class EmulatorHelper implements MemoryFaultHandler, EmulatorConfiguration
 			bytes = LittleEndianDataConverter.INSTANCE.getBytes(value, size);
 		}
 		writeMemory(stackMemorySpace.getAddress(offset), bytes);
+	}
+
+	public long getFieldPointer(long objectPointer, String token, int size) throws InsufficientBytesException {
+		return managedMemory.getFieldPointer(objectPointer, token, size);
+	}
+
+	public void setManagedHeapAddress(Address rangeStart, int byteSize) throws AddressOverflowException {
+		managedMemory.constructMallocMgr(rangeStart, byteSize);
 	}
 
 	/**
