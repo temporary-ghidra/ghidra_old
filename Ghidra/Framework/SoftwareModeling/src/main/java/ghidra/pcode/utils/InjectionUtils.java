d0325dc3e6fd3999330388ef1db70d82df6cd9b0
Danil6969 <20danil02@mail.ru>
2020-06-25 22:54:52 +0300

    Update InjectionUtils.java

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcode/utils/InjectionUtils.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/pcode/utils/InjectionUtils.java
@@ -31,6 +31,8 @@ import ghidra.program.model.pcode.PcodeOp;
 import ghidra.program.model.pcode.Varnode;
 import ghidra.program.model.symbol.Symbol;
 
+import java.util.ArrayList;
+
 public class InjectionUtils {
 
 	//All methods return either non-zero length arrays or null
@@ -38,7 +40,7 @@ public class InjectionUtils {
 	public static PcodeOp[] getEntryPcodeOps (Instruction instr) {
 		Program program = instr.getProgram();
 		Function func = program.getFunctionManager().getFunctionAt(instr.getMinAddress());
-		if(func != null) {
+		if (func != null) {
 			PrototypeModel callingConvention = func.getCallingConvention();
 			if (callingConvention == null) {
 				callingConvention = program.getCompilerSpec().getDefaultCallingConvention();
@@ -65,32 +67,93 @@ public class InjectionUtils {
 	public static PcodeOp[] getReturnPcodeOps (Instruction instr, PcodeOp pcode) {
 		Program program = instr.getProgram();
 		Varnode val = pcode.getInput(0);
-		Address target;
+		String nm = null;
+		Address target = null;
 		if (pcode.getOpcode() == PcodeOp.CALL) {
 			target = val.getAddress();
 		}
 		else if (pcode.getOpcode() == PcodeOp.CALLIND) {
-			if (val.isAddress()) {
-				PcodeInjectLibrary snippetLibrary = program.getCompilerSpec().getPcodeInjectLibrary();
-				Symbol[] symbols = program.getSymbolTable().getSymbols(instr.getMinAddress());
-				String nm = "";
-				for (Symbol sym : symbols) {
-					DataTypeSymbol datsym = HighFunctionDBUtil.readOverride(sym);
-					if (datsym != null) {
-						FunctionSignature dt = (FunctionSignature) datsym.getDataType();
-						nm = dt.getGenericCallingConvention().getDeclarationName();
-					}
-				}
-				String injectionName = nm + "@@inject_uponreturn";
-				InjectPayload payload = snippetLibrary.getPayload(InjectPayload.CALLMECHANISM_TYPE, injectionName, program, null);
-				InjectContext con = snippetLibrary.buildInjectContext();
-				con.baseAddr = instr.getMinAddress();
-				con.nextAddr = con.baseAddr.add(instr.getDefaultFallThroughOffset());
-				PcodeOp[] pcodeOps = payload.getPcode(program, con);
-				if(pcodeOps.length == 0)
-					pcodeOps = null;
-				return pcodeOps;
+			if (val.isConstant()) {
+				target = instr.getAddress().getNewAddress(val.getOffset(), true);
 			}
+		}
+		if (target != null) {
+			Function func = program.getFunctionManager().getFunctionAt(target);
+			if (func != null) {
+				PrototypeModel callingConvention = func.getCallingConvention();
+				if (callingConvention == null) {
+					callingConvention = program.getCompilerSpec().getDefaultCallingConvention();
+				}
+				nm = callingConvention.getName();
+			}
+		}
+		if (pcode.getOpcode() == PcodeOp.CALL || pcode.getOpcode() == PcodeOp.CALLIND) {
+			Symbol[] symbols = program.getSymbolTable().getSymbols(instr.getMinAddress());
+			for (Symbol sym : symbols) {
+				DataTypeSymbol datsym = null;
+				try {
+					datsym = HighFunctionDBUtil.readOverride(sym);
+				}
+				catch (IllegalArgumentException e) {}
+				if (datsym != null) {
+					FunctionSignature dt = (FunctionSignature) datsym.getDataType();
+					nm = dt.getGenericCallingConvention().getDeclarationName();
+				}
+			}
+		}
+		if (nm != null) {
+			PcodeInjectLibrary snippetLibrary = program.getCompilerSpec().getPcodeInjectLibrary();
+			String injectionName = nm + "@@inject_uponreturn";
+			InjectPayload payload = snippetLibrary.getPayload(InjectPayload.CALLMECHANISM_TYPE, injectionName, program, null);
+			if (payload == null) {
+				return null;
+			}
+			InjectContext con = snippetLibrary.buildInjectContext();
+			con.baseAddr = instr.getMinAddress();
+			con.nextAddr = con.baseAddr.add(instr.getDefaultFallThroughOffset());
+			PcodeOp[] pcodeOps = payload.getPcode(program, con);
+			if(pcodeOps.length == 0)
+				pcodeOps = null;
+			return pcodeOps;
+		}
+		return null;
+	}
+
+	public static PcodeOp[] getCallotherPcodeOps (Instruction instr, PcodeOp pcode) {
+		if (pcode.getOpcode() == PcodeOp.CALLOTHER) {
+			Program program = instr.getProgram();
+			PcodeInjectLibrary snippetLibrary = program.getCompilerSpec().getPcodeInjectLibrary();
+			String injectionName = program.getLanguage().getUserDefinedOpName((int) pcode.getInput(0).getOffset());
+			ArrayList<Varnode> inputs = new ArrayList();
+			StringBuilder sb = new StringBuilder();
+			sb.append("<context>\n");
+			sb.append("<addr space=\"" + instr.getAddress().getAddressSpace().getName());
+			sb.append("\" offset=\"" + "0x" + instr.getAddress().toString(false).replaceFirst("^0+(?!$)", ""));
+			sb.append("\"/><addr/><input>\n");
+			for (int i = 1; i < pcode.getNumInputs(); i++) {
+				inputs.add(pcode.getInput(i));
+				sb.append("<addr space=\"" + pcode.getInput(i).getAddress().getAddressSpace().getName());
+				sb.append("\" offset=\"" + "0x" + pcode.getInput(i).getAddress().toString(false).replaceFirst("^0+(?!$)", ""));
+				sb.append("\" size=\"" + pcode.getInput(i).getSize() + "\"/>\n");
+			}
+			sb.append("</input>\n</context>\n");
+			String context = sb.toString();
+			InjectPayload payload = snippetLibrary.getPayload(InjectPayload.CALLOTHERFIXUP_TYPE, injectionName, program, context);
+			if (payload == null) {
+				return null;
+			}
+			InjectContext con = snippetLibrary.buildInjectContext();
+			con.baseAddr = instr.getMinAddress();
+			con.nextAddr = con.baseAddr.add(instr.getDefaultFallThroughOffset());
+			con.inputlist = inputs;
+			if (pcode.getOutput() != null) {
+				con.output = new ArrayList();
+				con.output.add(pcode.getOutput());
+			}
+			PcodeOp[] pcodeOps = payload.getPcode(program, con);
+			if (pcodeOps.length == 0)
+				pcodeOps = null;
+			return pcodeOps;
 		}
 		return null;
 	}
